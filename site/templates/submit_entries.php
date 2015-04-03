9c368cd76feac59f6086da7a1f67b0d708f94b9e
Ariel Harlap <aharlap@gmail.com>
2015-04-03 13:21:32 -0400

    minor format change to submission form code

--- site/templates/submit_entries.php
+++ site/templates/submit_entries.php
@@ -9,39 +9,38 @@
 			<form class="form-horizontal" action="//formspree.io/boygeorge@technoculture.club" method="post">
 
 
-			<?php foreach($page->children() as $entry): ?>
+				<?php foreach($page->children() as $entry): ?>
 
-			<div class="form-group">
+				<div class="form-group">
 
-				<?php if ($entry->type() == "checkbox"): ?>
+					<?php if ($entry->type() == "checkbox"): ?>
 
-			    <div class="col-sm-offset-2 col-sm-10">
-					<div class="checkbox">
-				  		<label for="<?php echo $entry->title() ?>">
-						    <input type="checkbox" value="" id="<?php echo $entry->title() ?>">
-						    <?php echo $entry->label() ?>
-					    </label>
-					</div>	
+				    <div class="col-sm-offset-2 col-sm-10">
+						<div class="checkbox">
+					  		<label for="<?php echo $entry->title() ?>">
+							    <input type="checkbox" value="" id="<?php echo $entry->title() ?>">
+							    <?php echo $entry->label() ?>
+						    </label>
+						</div>	
+					</div>
+
+					<?php else: ?> <!-- not a checkbox type -->
+
+
+					<label for="<?php echo $entry->title() ?>" class="col-sm-2 control-label"><?php echo $entry->label() ?></label>
+					<div class="col-sm-10">
+						<input type="<?php echo $entry->type() ?>" class="form-control" id="<?php echo $entry->title() ?>" placeholder="<?php echo $entry->placeholder() ?>">
+					</div>
+
+				<?php endif ?> 
+
 				</div>
 
-				<?php else: ?> <!-- not a checkbox type -->
+		        <?php endforeach ; ?>
 
-
-				<label for="<?php echo $entry->title() ?>" class="col-sm-2 control-label"><?php echo $entry->label() ?></label>
-				<div class="col-sm-10">
-					<input type="<?php echo $entry->type() ?>" class="form-control" id="<?php echo $entry->title() ?>" placeholder="<?php echo $entry->placeholder() ?>">
+				<div class="col-sm-offset-2 col-sm-10">
+					<button type="submit" class="btn btn-default" name="Submit"><?php echo l::get('submit') ?></button>
 				</div>
-
-			<?php endif ?> 
-
-			</div>
-
-	        <?php endforeach ; ?>
-
-
-			<div class="col-sm-offset-2 col-sm-10">
-				<button type="submit" class="btn btn-default" name="Send"><?php echo l::get('submit') ?></button>
-			</div>
 			
 			</form>
 
