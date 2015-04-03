0b0af4677c01eef56472d246fcbffbdd29ad6eb4
Ariel Harlap <aharlap@gmail.com>
2015-04-03 13:11:38 -0400

    added formatting for checkbox input type and updated order of form fields

--- site/templates/submit_entries.php
+++ site/templates/submit_entries.php
@@ -12,10 +12,28 @@
 			<?php foreach($page->children() as $entry): ?>
 
 			<div class="form-group">
+
+				<?php if ($entry->type() == "checkbox"): ?>
+
+			    <div class="col-sm-offset-2 col-sm-10">
+					<div class="checkbox">
+				  		<label for="<?php echo $entry->title() ?>">
+						    <input type="checkbox" value="" id="<?php echo $entry->title() ?>">
+						    <?php echo $entry->label() ?>
+					    </label>
+					</div>	
+				</div>
+
+				<?php else: ?> <!-- not a checkbox type -->
+
+
 				<label for="<?php echo $entry->title() ?>" class="col-sm-2 control-label"><?php echo $entry->label() ?></label>
 				<div class="col-sm-10">
 					<input type="<?php echo $entry->type() ?>" class="form-control" id="<?php echo $entry->title() ?>" placeholder="<?php echo $entry->placeholder() ?>">
 				</div>
+
+			<?php endif ?> 
+
 			</div>
 
 	        <?php endforeach ; ?>
