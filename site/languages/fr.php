8192dc123c7245245198527b47e24800a6eda17e
Ariel Harlap <aharlap@gmail.com>
2015-04-03 13:20:22 -0400

    removed obsolete translation variables

--- site/languages/fr.php
+++ site/languages/fr.php
@@ -3,11 +3,4 @@
 l::set('about', 'À propos');
 l::set('submit', 'Soumettre');
 
-l::set('inputName', 'Your Name');
-l::set('inputNamePlaceholder', 'Enter your first and last name');
-l::set('inputEmail', 'Email address');
-l::set('inputEmailPlaceholder', 'Enter your email address');
-l::set('inputProjectTitle', 'Project Title');
-l::set('inputProjectTitlePlaceholder', 'Enter the title of your project');
-
 ?>
