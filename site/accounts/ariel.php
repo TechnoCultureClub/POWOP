e51aba5dc48e5aa82dca86750769a6ff47287673
Ariel Harlap <aharlap@gmail.com>
2015-04-14 14:51:06 -0400

    updated password and added profile picture

--- site/accounts/ariel.php
+++ site/accounts/ariel.php
@@ -5,6 +5,7 @@ firstname: Ariel
 lastname: Harlap
 email: aharlap@gmail.com
 password: >
-  $2a$10$chilXR4cxEpEUZBIsgjqQOk9E9R7etRALo9Jz56os0OSnd8brIQvS
+  $2a$10$9iSNcJdJJ2FqepIt/FpCL.GcB20Kpi24hLXkYUWrc5PD.xPQVFSDK
 language: en
 role: admin
+token: 59c1cfdb58f17005726dc118934dc5bfdb7493d5
